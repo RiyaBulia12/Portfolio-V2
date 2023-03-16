48510c75bce5195d14af779ff6e5a79f1e464fc2
riyabulia12 <riyabulia12@gmail.com>
2023-03-16 19:24:32 +0530

    Initial project setup

---
+++ assets/index.js
@@ -0,0 +1,38 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+};
